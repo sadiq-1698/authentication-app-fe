f5fe1b35a3784f8aeee6537dc08977e8b4b8a49f
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-07-03 21:54:08 +0530

    updated global styles

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import "./App.css";
-import Button from "./components/Button/Button";
-import InputField from "./components/InputField/InputField";
-import { MAIL_ICON, LOCK_ICON } from "./globals/constants";
-import SocialIcon, {
-  GithubIcon,
-  GoogleIcon
-} from "./components/SocialIcon/SocialIcon";
+import Routes from "./routes/Routes";
 
 function App() {
   return (
     <div className="App">
-      <h1>Components</h1>
-      <div style={{ width: "400px", margin: "10px" }}>
-        <Button text="Login" />
-      </div>
-      <div style={{ width: "400px", margin: "10px" }}>
-        <InputField icon={MAIL_ICON} iconPrefix placeholder="Email" />
-      </div>
-      <div style={{ width: "400px", margin: "10px" }}>
-        <InputField icon={LOCK_ICON} iconPrefix placeholder="Password" />
-      </div>
-      <div style={{ width: "400px", margin: "10px", display: "flex" }}>
-        <SocialIcon onClick={() => {}}>
-          <GithubIcon />
-        </SocialIcon>
-        <SocialIcon onClick={() => {}}>
-          <GoogleIcon />
-        </SocialIcon>
-      </div>
+      <Routes />
     </div>
   );
 }
