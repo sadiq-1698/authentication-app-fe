d9372ddb999c2778d112ecac559e2d5f6c7016a1
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-07-07 23:12:22 +0530

    updated topbar styles

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,9 +1,18 @@
 import NoProfilePic from "../../static/images/noProfilePic.svg";
 import DevChallengesLogo from "../../static/images/devchallenges.svg";
 
+import Dropdown from "../Dropdown/Dropdown";
+
 import "./styles.css";
+import useComponentVisible from "../../hooks/useComponentVisible";
 
 const TopBar = () => {
+  const { ref, isComponentVisible, toggleVisibility } = useComponentVisible();
+  const triggerDropdown = e => {
+    e.preventDefault();
+    toggleVisibility();
+  };
+
   return (
     <div className="tb-container">
       <div className="left">
@@ -13,20 +22,27 @@ const TopBar = () => {
           className="dev-ch-logo"
         />
       </div>
-      <div className="right">
-        <div className="img-container">
-          <img
-            src={NoProfilePic}
-            alt="no-profile-img"
-            className="no-profile-img"
-            width="100%"
-            height="100%"
-          />
-        </div>
+      <a href="/" onClick={triggerDropdown} ref={ref}>
+        <div className="right">
+          <div className="img-container">
+            <img
+              src={NoProfilePic}
+              alt="no-profile-img"
+              className="no-profile-img"
+              width="100%"
+              height="100%"
+            />
+          </div>
 
-        <p className="profile-name">User name</p>
-        <div className="arrow-down"></div>
-      </div>
+          <p className="profile-name">User name</p>
+          <div
+            className={`arrow-down ${!isComponentVisible ? "rotate2" : ""} ${
+              isComponentVisible ? "rotate" : ""
+            }`}
+          ></div>
+          {isComponentVisible && <Dropdown />}
+        </div>
+      </a>
     </div>
   );
 };
