aec762b373527a45fb279563b53a0c15d2a65ef8
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-07-10 15:32:05 +0530

    added firebase config

---
+++ src/firebase/authMethods.js
@@ -0,0 +1,4 @@
+import firebase from "./config";
+
+export const githubAuthProvider = new firebase.auth.GithubAuthProvider();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzbpDLdJ216gdcWXTXladnJZRre6WAABE",
+  authDomain: "authentication-app-319310.firebaseapp.com",
+  projectId: "authentication-app-319310",
+  storageBucket: "authentication-app-319310.appspot.com",
+  messagingSenderId: "774362402204",
+  appId: "1:774362402204:web:244b0d3f75b10cadab0c59"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
