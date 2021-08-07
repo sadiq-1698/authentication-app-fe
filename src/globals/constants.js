97150df90ee68f4fed748a4004736d172d2a9052
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-08-07 17:27:53 +0530

    adding constants for api endpoints

--- src/globals/constants.js
+++ src/globals/constants.js
@@ -1,3 +1,22 @@
+export const API_BASE_URL = process.env.REACT_APP_BASE_API_URL;
+
+export const API_ENDPOINTS = {
+  user: {
+    regsister: `${API_BASE_URL}/user/register`,
+    login: `${API_BASE_URL}/user/login`,
+    editProfile: `${API_BASE_URL}/user/editProfile`,
+    getAllUsers: `${API_BASE_URL}/users`,
+    google: {
+      login: `${API_BASE_URL}/user/google/login`,
+      register: `${API_BASE_URL}/user/google/register`
+    },
+    github: {
+      login: `${API_BASE_URL}/user/github/login`,
+      register: `${API_BASE_URL}/user/github/register`
+    }
+  }
+};
+
 // ICON CLASSES
 export const ICONS = {
   MAIL: "fa fa-envelope",
